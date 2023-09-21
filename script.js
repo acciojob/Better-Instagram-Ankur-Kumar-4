18a6bf9e58200f506747e6ac691a93356685063c
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-09-21 21:44:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your code here
+const image_div = document.getElementsByClassName("image");
+let sourceImageId = null;
+
+function dragstart(event) {
+    sourceImageId = event.target.id;
+}
+
+for (let i = 0; i < image_div.length; i++) {
+    image_div[i].addEventListener("dragstart", dragstart);
+}
+
+function dragover(event) {
+    event.preventDefault();
+}
+
+for (let i = 0; i < image_div.length; i++) {
+    image_div[i].addEventListener("dragover", dragover);
+}
+
+function drop(event) {
+    event.preventDefault();
+    const targetImage = event.target;
+    const targetImageId = targetImage.id;
+
+    
+    event.target.id = sourceImageId;
+    document.getElementById(sourceImageId).id = targetImageId;
+
+    
+    sourceImageId = null;
+}
+
+for (let i = 0; i < image_div.length; i++) {
+    image_div[i].addEventListener("drop", drop);
+}
